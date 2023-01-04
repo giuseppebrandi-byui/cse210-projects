5c29935b1dc007bc0a4e706c1f2e6d030e1750fa
giuseppebrandi-byui <bra22041@byui.edu>
2023-01-04 21:22:52 +0000

    Added space between lines

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,7 +10,6 @@ class Program
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine("");
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
