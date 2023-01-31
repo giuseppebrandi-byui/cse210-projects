6e0167a93b25fe803677d0fed7ee5377ca67aba1
giuseppebrandi-byui <bra22041@byui.edu>
2023-01-31 22:20:38 +0000

    Week 5 Preparation activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,19 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 10));
+        shapes.Add(new Rectangle("yellow", 10, 20));
+        shapes.Add(new Circle("blue", 6));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
